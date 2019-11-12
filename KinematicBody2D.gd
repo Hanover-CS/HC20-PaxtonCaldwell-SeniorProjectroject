f3fc996c03f21d88c0b387e4bf8837e0027ec90c
Paxton Caldwell <caldwellp20@hanover.edu>
2019-11-12 15:00:06 -0500

    First Commit

---
+++ KinematicBody2D.gd
@@ -0,0 +1,33 @@
+extends KinematicBody2D
+
+const GRAVITY = 20
+const SPEED = 200
+const JUMP_HEIGHT = -550
+const UP = Vector2(0,-1)
+
+var motion = Vector2()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta):
+	motion.y += GRAVITY
+
+	if Input.is_action_pressed("ui_right"):
+		motion.x = SPEED
+		$Sprite.flip_h = false
+		$Sprite.play("Run")
+	elif Input.is_action_pressed("ui_left"):
+		motion.x = -SPEED
+		$Sprite.flip_h = true
+		$Sprite.play("Run")
+	else:
+		$Sprite.play("Idle")
+		motion.x = 0
+	
+	if is_on_floor():
+		if Input.is_action_just_pressed("ui_up"):
+			motion.y = JUMP_HEIGHT
+	motion = move_and_slide(motion, UP)
+	
+	pass
+
+
