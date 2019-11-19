773f04c0d4b3d01437473bf10861e460d7e1c9a6
Paxton Caldwell <caldwellp20@hanover.edu>
2019-11-19 15:44:58 -0500

    Commit

--- KinematicBody2D.gd
+++ KinematicBody2D.gd
@@ -26,6 +26,8 @@ func _physics_process(delta):
 	if is_on_floor():
 		if Input.is_action_just_pressed("ui_up"):
 			motion.y = JUMP_HEIGHT
+	else:
+		$Sprite.play("Jump")
 	motion = move_and_slide(motion, UP)
 	
 	pass
