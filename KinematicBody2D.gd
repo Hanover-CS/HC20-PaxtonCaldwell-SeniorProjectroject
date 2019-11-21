7edc82bbb7e8a119b866793d8b2e2ce0ec412968
Paxton Caldwell <caldwellp20@hanover.edu>
2019-11-20 19:06:55 -0500

    Added acceleration to character movement and refactored code

--- KinematicBody2D.gd
+++ KinematicBody2D.gd
@@ -1,7 +1,8 @@
 extends KinematicBody2D
 
 const GRAVITY = 20
-const SPEED = 200
+const MAX_SPEED = 200
+const ACCELERATION = 50
 const JUMP_HEIGHT = -550
 const UP = Vector2(0,-1)
 
@@ -10,26 +11,36 @@ var motion = Vector2()
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
 	motion.y += GRAVITY
-
-	if Input.is_action_pressed("ui_right"):
-		motion.x = SPEED
-		$Sprite.flip_h = false
-		$Sprite.play("Run")
-	elif Input.is_action_pressed("ui_left"):
-		motion.x = -SPEED
-		$Sprite.flip_h = true
-		$Sprite.play("Run")
-	else:
-		$Sprite.play("Idle")
-		motion.x = 0
+	move_character()
+	on_floor_ready_to_jump()
+	motion = move_and_slide(motion, UP)
+	pass
 	
+func move_player_right():
+	motion.x = min(motion.x+ACCELERATION, MAX_SPEED)
+	$Sprite.flip_h = false
+	$Sprite.play("Run")
+
+func move_player_left():
+	motion.x = max(motion.x-ACCELERATION, -MAX_SPEED)
+	$Sprite.flip_h = true
+	$Sprite.play("Run")
+	
+func stop_player_movement():
+	$Sprite.play("Idle")
+	motion.x = 0
+		
+func on_floor_ready_to_jump():
 	if is_on_floor():
 		if Input.is_action_just_pressed("ui_up"):
 			motion.y = JUMP_HEIGHT
 	else:
 		$Sprite.play("Jump")
-	motion = move_and_slide(motion, UP)
-	
-	pass
 
-
+func move_character():
+	if Input.is_action_pressed("ui_right"):
+		move_player_right()
+	elif Input.is_action_pressed("ui_left"):
+		move_player_left()
+	else:
+		stop_player_movement()
