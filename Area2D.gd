aa66372d360f3ac48a80ff6d12fe020fcc3d71f2
Paxton Caldwell <caldwellp20@hanover.edu>
2020-01-28 13:54:47 -0500

    Back Button on level selection menu and and end points on levels

--- Area2D.gd
+++ Area2D.gd
@@ -1,10 +1,8 @@
 ##WorldComplete.gd
 extends Area2D
 
-export(String, FILE, "*.tscn") var world_scene
-
 func _physics_process(delta):
 	var bodies = get_overlapping_bodies()
 	for body in bodies:
 		if body.name == "player":
-			get_tree().change_scene(world_scene)
+			get_tree().change_scene("res://LevelSelectMenu.tscn")
--- LevelSelectMenu.gd
+++ LevelSelectMenu.gd
@@ -3,3 +3,9 @@ extends Control
 
 func _on_Level1Button_pressed():
 	get_tree().change_scene("res://World.tscn")
+
+func _on_Level2Button_pressed():
+	get_tree().change_scene("res://World2.tscn")
+
+func _on_BackButton_pressed():
+	get_tree().change_scene("res://StartMenu.tscn")
