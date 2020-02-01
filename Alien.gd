1a77d8f730fdcdbf235fdabe92042edfa7eb955e
Paxton Caldwell <caldwellp20@hanover.edu>
2020-02-01 08:37:06 -0500

    changed camera size and alien AI can detect walls and change direction

--- Alien.gd
+++ Alien.gd
@@ -5,10 +5,14 @@ const SPEED = 30
 const FLOOR = Vector2(0, -1)
 
 var velocity = Vector2()
+var direction = 1
 
 func _physics_process(delta):
-	velocity.x = SPEED
+	velocity.x = SPEED * direction
 	$AnimatedSprite.play("WalkA")
 	velocity.y += GRAVITY
 	velocity = move_and_slide(velocity, FLOOR)
+	
+	if is_on_wall():
+		direction = direction * -1
 
