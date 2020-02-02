5f749d98af8d55e1ab89b58ba77cd1bdd5ef9ff6
Paxton Caldwell <caldwellp20@hanover.edu>
2020-02-01 19:34:42 -0500

    changed level1 and level2 and added alien instance

--- Alien.gd
+++ Alien.gd
@@ -9,10 +9,22 @@ var direction = 1
 
 func _physics_process(delta):
 	velocity.x = SPEED * direction
+	if direction == 1:
+		$AnimatedSprite.flip_h = false
+	else:
+		$AnimatedSprite.flip_h = true
+		
 	$AnimatedSprite.play("WalkA")
 	velocity.y += GRAVITY
 	velocity = move_and_slide(velocity, FLOOR)
 	
 	if is_on_wall():
 		direction = direction * -1
+		$RayCast2D.position.x *= -1
+		
+	if $RayCast2D.is_colliding() == false:
+		direction = direction * -1
+		$RayCast2D.position.x *= -1
+	
+	
 
