5c20adae0b6318c07eb2e235c50401c2742d906a
Paxton Caldwell <caldwellp20@hanover.edu>
2020-02-01 20:44:08 -0500

    Added Level3

--- LevelSelectMenu.gd
+++ LevelSelectMenu.gd
@@ -9,6 +9,11 @@ func _on_Level1Button_pressed():
 
 func _on_Level2Button_pressed():
 	get_tree().change_scene("res://Level2.tscn")
+	
+func _on_Level3Button_pressed():
+	get_tree().change_scene("res://Level3.tscn")
 
 func _on_BackButton_pressed():
 	get_tree().change_scene("res://StartMenu.tscn")
+
+
